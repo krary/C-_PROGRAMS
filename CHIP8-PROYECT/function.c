c30ae675772c9a3b86bcd77e6f1c65bbbe4b9b3a
krary <fraggmentado@gmail.com>
2026-08-29 20:22:03 +0200

    upgrade

--- CHIP8-PROYECT/function.c
+++ CHIP8-PROYECT/function.c
@@ -27,25 +27,82 @@ if(f)fclose(f);}
 void init_chip8_cycle(Chip8 *ch){
 uint16_t opcode = (ch->ram[ch->pc] << 8) 
 | ch->ram[ch->pc+1];
+ch->pc +=2;
 uint8_t x = (opcode & 0x0f00)>>8;
 uint8_t y = (opcode & 0x00f0)>>4;
 uint8_t n = (opcode & 0x000f);
 uint8_t kk = (opcode & 0x00ff);
-uint8_t nnn = (opcode & 0x0fff);
+uint16_t nnn = (opcode & 0x0fff);
 
 switch(opcode &0xf000){
+    case 0xD000:{
+    	uint8_t x_pos = ch->V[x] % 64;
+    	uint8_t y_pos = ch->V[y] % 32;
+    	ch->V[0xF]=0;
+    	for(uint8_t row = 0; row < n; row++){
+    		uint8_t sprite_byte = ch->ram[ch->I + row];
+    		for(uint8_t col = 0; col < 8;col++){
+    			uint8_t sprite_pixel = sprite_byte &(0x80 >> col);
+    			if(sprite_pixel != 0){
+    				uint16_t screen_x = (x_pos + col) % 64;
+    				uint16_t screen_y = (y_pos + row) % 32;
+    				uint16_t index = screen_y * 64 + screen_x;
+    			    if(ch->display[index] == 1){ch->V[0XF]=1;}
+    			    ch->display[index] ^= 1;	
+    			}
+    		}
+    	}
+    	break;
+    }
 	case 0x0000:
-	if(opcode == 0x0E00){
+	if(opcode == 0x00E0){
 memset(ch->display,0,sizeof(ch->display));}
     else if(opcode == 0x00EE){//CALL RETURN
     	ch->sp--;
     	ch->pc = ch->stack[ch->sp];}
     break;
-    case 0x1000:
+    case 0x1000://SALTO INCONDICIONAL A NNN:
         ch->pc = nnn;
         break; 
-     case 0x6000:
+     case 0x2000:
+        ch->stack[ch->sp]= ch->pc;
+        ch->sp++;
+        ch->pc = nnn;
+        break;
+     case 0x3000:
+        if(ch->V[x]==kk){ch->pc +=2;}
+        break;
+     case 0x4000:
+        if(ch->V[x] != kk){ch->pc+=2;}
+        break;
+     case 0x6000: //CREAR UN VALOR EN EL REGISTRO V[X]: kk
         ch->V[x]=kk;
+        break;
+     case 0x7000: //SUMAR UN VALOR EN EL REGISTRO V[x]: kk
+        ch->V[x] += kk;
+        break;
+     case 0x8000:
+        switch(n){
+        	case 0x0:ch->V[x] = ch->V[y];break;
+        	case 0x1:ch->V[x] |= ch->V[y];break; //EXPRESION OR UN UNO CUANDO HAY UN UNO
+        	case 0x2:ch->V[x] &= ch->V[y];break; //EXPRESION MASCARA UN UNO CUANDO AMBOS SON UNO
+        	case 0x3:ch->V[x] ^= ch->V[y];break; //EXPRESION XOR UN UNO CUANDO AMBOS SON DIFERENTES
+        	case 0x4:{
+        		uint16_t sum = ch->V[x] + ch->V[y];
+        		ch->V[0XF] = (sum>255)?1:0;
+        		ch->V[x] = sum & 0xFF;
+        		break;}
+        	case 0x5:{
+        		ch->V[0xF] = (ch->V[x]>= ch->V[y])?1:0;
+        		ch->V[x] -= ch->V[y];
+        		break;}
+        }
+     break;
+     case 0xA000:
+        ch->I = nnn;
+        break;
+     default:
+        printf("OPCODE UKNOWN : 0X%04X\n",opcode);
         break;
  	}
 }
