bbf4895282e4d0dba76aaf2a0fba0a5841800b2e
krary <fraggmentado@gmail.com>
2026-09-18 17:03:38 +0000

    upgrade

---
+++ EXAMPLES_CODE/TCP_NETCAT/library.h
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netdb.h>
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+
+#define PORT 80
+#define BUFFER_SIZE 4096
+
+
+
+
+
+
+#endif
