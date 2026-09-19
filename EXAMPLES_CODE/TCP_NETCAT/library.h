e2f2830303b1d6889335169979314db4c31d1be4
krary <fraggmentado@gmail.com>
2026-09-19 11:52:16 +0200

    upgrade

--- EXAMPLES_CODE/TCP_NETCAT/library.h
+++ EXAMPLES_CODE/TCP_NETCAT/library.h
@@ -2,18 +2,23 @@
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
+#include<netinet/tcp.h>
+#include<netinet/ip.h>
 #include<netdb.h>
+#include<arpa/inet.h>
 #ifndef LIBRARY_H
 #define LIBRARY_H 
 
 
 #define PORT 80
-#define BUFFER_SIZE 4096
+#define DATAGRAM_LEN 4096
 
-
-
+extern struct iphdr ip_header;
+extern struct tcphdr tcp_header;
+extern char datagram[DATAGRAM_LEN];
+void init_ip(struct iphdr *);
+void init_tcp(struct tcphdr *);
+void init_datagram_ip_tcp(char*);
 
 
 
--- EXAMPLES_CODE/TCP_NETCAT/tcp_netcat_function.c
+++ EXAMPLES_CODE/TCP_NETCAT/tcp_netcat_function.c
@@ -1,1 +1,27 @@
 #include "library.h"
+
+/*1[+]DEFINIDMOS LAS CABEZERAS DE ------- IP       void init_ip()  */
+/*2[+]DEFINIDMOS LAS CABEZERAS DE ------- TCP      void init_tcp   */
+/*3[+]UNIMOS LAS CABEZERAS DE IP + TCP Y LO METEMOS DENTRO DE UN BUFFER  void init_buffer_header()  */
+
+/*************************CABEZERAS PERSONALIZADAS *****************************/
+
+void init_ip(struct iphdr *i){
+//CONFIGURANDO LAS CABEZERAS IP MANUALMENTE
+	i->version = 4; // IPV4;
+	i->ihl     = 5; //Tamaño de cabezera 5;
+	i->protocol = IPPROTO_TCP; // Indica que adentro va el TCP;
+    i->saddr = inet_addr("192.168.1.2"); //Ip fuente definida real o no
+    i->daddr = inet_addr("1.1.1.1");	 //IP destino;
+ }
+void init_tcp(struct tcphdr *t){
+//CONFIGURANDO LAS CABEZERAS TCP MANUALMENTE
+	t->source = htons(12345); // Puerto de origen; 
+	t->dest =   htons(80);    // Puerto de destino;
+    t->syn  =    1;            //Activar la bandera SYN(Inicio de conexión )
+    }
+
+void init_datagram(char *d){
+memset(d,0,DATAGRAM_LEN);
+	
+}
